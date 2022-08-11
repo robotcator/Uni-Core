4bd4a7a1549ee3a2cc787bc0a2e5e7f6b2032d9e
robotcator <problemset@163.com>
2022-08-11 17:43:43 +0800

    use default python

--- .github/workflows/env.sh
+++ .github/workflows/env.sh
@@ -46,8 +46,8 @@ sudo    apt-get install -y --no-install-recommends \
         debhelper \
         nfs-common
 
-wget -O ~/miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh  && \
-     chmod +x ~/miniconda.sh && \
-     ~/miniconda.sh -b -p /opt/conda && \
-     rm ~/miniconda.sh
-export PATH=/opt/conda/bin:$PATH
+# wget -O ~/miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh  && \
+#      chmod +x ~/miniconda.sh && \
+#      ~/miniconda.sh -b -p /opt/conda && \
+#      rm ~/miniconda.sh
+# export PATH=/opt/conda/bin:$PATH
