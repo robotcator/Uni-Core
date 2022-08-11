ed5ec139ff9bb009dcb64cad3027d18f2b30c48c
robotcator <problemset@163.com>
2022-08-11 15:56:43 +0800

    add conda env

--- .github/workflows/env.sh
+++ .github/workflows/env.sh
@@ -45,3 +45,9 @@ sudo    apt-get install -y --no-install-recommends \
         devscripts \
         debhelper \
         nfs-common
+
+wget -O ~/miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh  && \
+     chmod +x ~/miniconda.sh && \
+     ~/miniconda.sh -b -p /opt/conda && \
+     rm ~/miniconda.sh
+export PATH=/opt/conda/bin:$PATH
